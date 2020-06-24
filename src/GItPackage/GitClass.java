01bb4f16b44ebf0812c89289417d3fe245785a65
ShruthiNarva <shruthinarva1@gmail.com>
2020-06-24 12:49:07 +0100

    second commit

--- src/GItPackage/GitClass.java
+++ src/GItPackage/GitClass.java
@@ -3,7 +3,7 @@ package GItPackage;
 public class GitClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello , how are you?");
+		System.out.println("Hello , how are you!");
 
 	}
 
