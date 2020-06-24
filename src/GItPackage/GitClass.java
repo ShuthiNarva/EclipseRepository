9b4eec0ba37a29e489c9652ae6a00081206251b9
ShruthiNarva <shruthinarva1@gmail.com>
2020-06-24 12:55:36 +0100

    New Commit

--- src/GItPackage/GitClass.java
+++ src/GItPackage/GitClass.java
@@ -5,7 +5,7 @@ public class GitClass {
 	public static void main(String[] args) {
 		System.out.println("Hello , how are you!");
 		System.out.println("My commit works");
-		
+		System.out.println("THird Commit");
 
 	}
 
